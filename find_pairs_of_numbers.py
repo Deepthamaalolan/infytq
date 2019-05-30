b582400bb547d6d515c49bf6ef4d41b7fdf2e6b5
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2019-05-30 18:18:49 +0530

    Create find_pairs_of_numbers.py

---
+++ find_pairs_of_numbers.py
@@ -0,0 +1,12 @@
+def find_pairs_of_numbers(num_list,n):
+    count=0
+    for i in range (0,len(num_list)-1):
+        for j in range(i+1,len(num_list)):
+            if num_list[i]+num_list[j]==n:
+                count=count+1 
+    return count
+    #Remove pass and write your logic here
+
+num_list=[1, 2, 4, 5, 6]
+n=6
+print(find_pairs_of_numbers(num_list,n))
