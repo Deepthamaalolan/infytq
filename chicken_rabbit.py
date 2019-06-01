c6230ccc255e2ac811f6274de75293b7b359c127
Deepthamaalolan <46663016+Deepthamaalolan@users.noreply.github.com>
2019-06-01 10:45:28 +0530

    Update chicken_rabbit.py

--- chicken_rabbit.py
+++ chicken_rabbit.py
@@ -1,14 +1,28 @@
-def solve(numheads,numlegs):
+def solve(heads,legs):
+    error_msg="No solution"
+    rabbit_count=0 
+    chicken_count=0 
+    rabbit_count=abs(legs-(heads*2))
+    
+    if(rabbit_count % 2==0):
+        rabbit_count=rabbit_count//2
+        chicken_count=abs(heads-rabbit_count)
+    if(chicken_count+rabbit_count==heads):
+        print(chicken_count,rabbit_count)
+    else:
+        print(error_msg)
 
-    ns='No solutions!'
-    for i in range(numheads+1):
-        j=numheads-i
-        if (2*i)+(4*j)==numlegs:
-            return i,j
-    return ns,ns
+    #Start writing your code here
+    #Populate the variables: chicken_count and rabbit_count
 
 
-numheads=35
-numlegs=94
-solutions=solve(numheads,numlegs)
-print (solutions)
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+
+    #print(chicken_count,rabbit_count)
+    #print(error_msg)
+
+#Provide different values for heads and legs and test your program
+solve(20,10)
+
